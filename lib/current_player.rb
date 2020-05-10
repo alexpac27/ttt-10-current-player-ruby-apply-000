bf9194cbd6a6f4e818d4246ccd153c7d08b073e2
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-10 00:51:53 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,10 +3,11 @@ board = [" "," "," "," "," "," "," "," "," "]
 def turn_count(board)
   counter = 0
   board.eac{ |space|
-  if space == "X" || space == "O"
-  counter +=1
-else
-counter = counter
-end }
+    if space == "X" || space == "O"
+      counter +=1
+    else
+      counter = counter
+    end
+  }
 return counter
 end
